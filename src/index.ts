6e5b4e5c67cf88373963a56b38d29607f0063443
yomi <adefuyeabayomi16@gmail.com>
2024-08-09 21:07:50 +0100

    initialize fst service library

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as authService } from './services/authService';
+export { default as profileService } from './services/profileService';
+export { default as pingService } from './services/pingService';
+export { default as customerService } from './services/customerService'
+
+export default {}
---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+// Define your production and local URLs
+const productionURL = "https://joegreen-express-server.onrender.com";
+const localURL = "http://localhost:8080";
+
+// Function to check if the application is running locally
+function backendUrl() {
+  console.log({LOCATION: window.location.hostname})
+  const hostname = window.location.hostname;
+  return hostname === "localhost" || hostname === "127.0.0.1" ? localURL : productionURL;
+}
+
+console.log(backendUrl())
+// Create an axios instance with the appropriate baseURL
+const axiosInstance = axios.create({
+  baseURL: backendUrl(),
+  headers: {
+    "Content-Type": "application/json" // Set the content type to JSON
+  }
+});
+
+export default axiosInstance;
